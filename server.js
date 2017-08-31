84edfe80e246937ecaa2c347fa7d6560f1d1b2f7
Prernakaler <ps00022@gmail.com>
2017-08-31 22:33:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req, res)
 {
-    var articleName = req.params.articlName;
+    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName])); 
 });
 
