7afeeb58cec2a287224a77219030804fc11a3587
Prernakaler <ps00022@gmail.com>
2017-09-01 05:52:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML = 'New value';
 
 //move the image
 var img = document.getElementById('madi');
+var marginLeft =0;
+function moveRight(){
+    marginLeft = marginLeft+10;
+    img.style.marginLeft =marginLeft='px';
+}
 img.onClick = function(){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRoght,100);   //every 100ms apply the move right function
+    
 };
