f3c3b552e95d457fbc37c2c41664cbf8c1d269b1
Prernakaler <ps00022@gmail.com>
2017-09-17 19:08:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.get('/test-db', function(req,res ){
          res.status(500).send(err.toString());   
         }
         else{
-            res.send(JSON>stingify(result));
+            res.send(JSON.stringify(result));
         }
         
     });
