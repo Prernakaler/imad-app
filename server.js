44d42da40a2c9f96868cf3ae0a9b296d579e056e
Prernakaler <ps00022@gmail.com>
2017-09-17 19:04:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = required('pg').Pool;
+
+var config = {
+    user: 'ps00022',
+    database: 'ps00022',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -130,6 +139,23 @@ var counter = 0;
 app.get('/counter', function(req,res){
     counter = counter+1;
     res.send(counter.toString());
+});
+
+var pool = new Pool(config);
+
+app.get('/test-db', function(req,res ){
+    //make a select request
+    //return a response with result
+    
+    pool.query('SELECT * FROM test', function(err, result) {
+        if(err){
+         res.status(500).send(err.toString());   
+        }
+        else{
+            res.send(JSON>stingify(result));
+        }
+        
+    });
 });
 
 app.get('/:articleName',function(req, res)
