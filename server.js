f3cff4072b51a4bd48dbb162f503536172672a6f
Prernakaler <ps00022@gmail.com>
2017-09-17 19:51:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,10 +158,21 @@ app.get('/test-db', function(req,res ){
     });
 });
 
-app.get('/:articleName',function(req, res)
+app.get('/articles/:articleName',function(req, res)
 {
-    var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName])); 
+    
+    
+    pool.query("SELECT * FROM article WHERE title ="+req.params.articleName, function(err, result){
+        if(err){
+                     res.status(500).send(err.toString());   
+        }else{
+           if(result.rows.length===0){
+               res.status(404).send(
+           }
+           
+        }
+    });
+   res.send(createTemplate(articleData)); 
 });
 
 /*app.get('/article-two',function(req,res)
